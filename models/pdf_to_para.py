3664efdac05a4128b60bff0d4e4fce56ad422be5
Sowmesh <sowmeshshailesh1234@gmail.com>
2024-03-09 16:07:22 +0530

    text_parsing!

--- models/pdf_to_para.py
+++ models/pdf_to_para.py
@@ -1,37 +1,53 @@
+import PyPDF2
+import easyocr
 import os
+import re
 
 
-def extract_paragraphs_from_text_file(text_file):
-    with open(text_file, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
+# def extract_text(filepath):
+#     pdf_reader = PyPDF2.PdfReader(filepath)
+#     paragraphs = []
+
+#     # Use len(reader.pages) for number of pages
+#     for page_num in range(len(pdf_reader.pages)):
+#         page = pdf_reader.pages[page_num]
+#         text = page.extract_text()
+
+#         # Split the text into paragraphs.
+#         paragraphs.extend(text.split("\n\n"))
+
+#     return paragraphs
+
+
+def extract_text_from_pdf_with_pdfreader(pdf_path):
+    reader = PyPDF2.PdfReader(pdf_path)
+    num_pages = len(reader.pages)
 
     paragraphs = []
-    current_paragraph = ''
-    for line in lines:
-        line = line.strip()  # Remove leading and trailing whitespaces
-        if line:  # If the line is not empty
-            current_paragraph += line + '\n'  # Add it to the current paragraph
-        else:  # If an empty line is encountered, consider it as the end of the paragraph
-            if current_paragraph:  # Ensure there is content in the paragraph
-                # Add the paragraph to the list
-                paragraphs.append(current_paragraph.strip())
-                current_paragraph = ''  # Reset the current paragraph for the next iteration
-
-    # Add the last paragraph if it's not empty
-    if current_paragraph:
-        paragraphs.append(current_paragraph.strip())
+    for page_num in range(num_pages):
+        page_text = reader.pages[page_num].extract_text()
+        paragraphs.extend(page_text.split("\n"))
 
     return paragraphs
 
 
-# Example usage:
+def find_parent_folder(filename, search_directory='.'):
+    for root, dirs, files in os.walk(search_directory):
+        if filename in files:
+            return os.path.abspath(root)
+
+    return None  # File not found in the specified directory and its subdirectories
+
+
 fname = r'C:\Users\sowme\StudioProjects\hashcode_backend\assets'
-text_file_path = os.path.join(fname, "PussInBoots.txt")
+output_file = os.path.join(fname, "Beauty_and_Beast.pdf")
 
-paragraphs = extract_paragraphs_from_text_file(text_file_path)
+paragraphs = extract_text_from_pdf_with_pdfreader(output_file)
 
-# Print the extracted paragraphs
-for i, para in enumerate(paragraphs):
-    print(f"Paragraph {i+1}:")
-    print(para)
-    print()
+# print(paragraphs)
+
+i = 0
+for para in paragraphs:
+    print(i)
+    print(para, "\n")
+    i += 1
