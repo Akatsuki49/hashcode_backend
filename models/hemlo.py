babfdc2c0371a17ce1e6bec33ad8c4b438d3ca08
Sowmesh Sharma <95576716+SowmeshSharma0411@users.noreply.github.com>
2024-03-10 00:16:32 +0530

    Create hemlo.py

---
+++ models/hemlo.py
@@ -0,0 +1,10 @@
+import torch
+from diffusers import StableDiffusionPipeline
+
+model_id = "dreamlike-art/dreamlike-photoreal-2.0"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
+pipe = pipe.to("cuda")
+    # return pipe
+
+# Save the model to a .pkl file
+torch.save(pipe, 'image-model.pkl')
